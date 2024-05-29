caee28b69fa7dedcef067d37b1fc4b9f9c857f8b
SterFryNoodle <revangcruz@yahoo.com>
2024-05-28 23:35:56 -0700

    Update survivor script; attach script to ally object

--- Assets/Scripts/SurvivorBehavior.cs
+++ Assets/Scripts/SurvivorBehavior.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SurvivorBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private NavMeshAgent allyAgent;
+
+    Transform allyTarget;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
     }
+
+
 }
